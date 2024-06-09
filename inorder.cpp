b865079516ced26c6e83c77153096ec8156cb578
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-06-09 18:04:20 +0530

    solved

---
+++ inorder.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    void solve(Node* root, vector<int>&ans){
+        if(root == NULL)
+            return;
+            
+        solve(root->left,ans);
+        ans.push_back(root->data);
+        solve(root->right,ans);
+    }
+    vector<int> inOrder(Node* root) {
+        
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+        
+    }
+};
